e51280289e435372ce61e7431352d23a7da7780f
Anton <dzanto@gmail.com>
2020-09-21 16:51:59 +0300

    Add models

---
+++ api_yamdb/models/content.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Categories(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.slug
+
+
+class Genres(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.slug
+
+
+class Titles(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.PositiveIntegerField(max_length=4)
+    description = models.TextField()
+    genre = models.ForeignKey(Genres, on_delete=models.CASCADE, related_name="titles")
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, related_name="titles")
